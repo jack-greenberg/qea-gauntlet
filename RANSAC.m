db74f773cf26ac3080d3b43bb42625f8a5f96d0b
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-05-02 18:51:26 -0700

    Finish gauntlet navigation challenge

--- RANSAC.m
+++ RANSAC.m
@@ -1,53 +1,75 @@
-function [bestInlierType, bestInlierSet, bestOutlierSet, m, b, bestEndPoints, C, R] = RANSAC(X, n, d)
-    bestInlierSet = [];
-    bestOutlierSet = [];
-    bestInlierType = 'line';
+% Identifies one feature of a set of points
+function [line, circle, outliers] = RANSAC(X, n, d)
+    disp(size(X));
     
-    C = zeros(0,2); R = 0;
-    m = 0; b = 0; bestEndPoints = zeros(0,2);
-
+    line = []; circle = [];
+    
+    % Current best set of inliers
+    bestInlierSet = [];
+    bestInlierSetIndex = [];
+    bestInlierType = 'none';
+    
     for i=1:n
+        % Get 3 random points
         candidates = datasample(X, 3, 'Replace', false);
         P1 = candidates(1,:);
         P2 = candidates(2,:);
         P3 = candidates(3,:);
         
-        [line_inliers, biggestGap] = lineDetector(X, d, P1, P2);
+        [line_inliers1, biggestGap1] = lineDetector(X, d, P1, P2);
+        [line_inliers2, biggestGap2] = lineDetector(X, d, P1, P3);
+        [line_inliers3, biggestGap3] = lineDetector(X, d, P2, P3);
+        [circle_inliers, xc, yc, r, biggestGapC] = circleDetector(X, d, P1, P2, P3);
         
-        if sum(line_inliers) > size(bestInlierSet, 1) && biggestGap < .2 && sum(line_inliers) > 10
+        if sum(line_inliers1) > size(bestInlierSet, 1) && biggestGap1 < .2 && sum(line_inliers1) > 10 % If the first line is better than our current best
+            bestInlierSet = X(line_inliers1,:);
+            bestInlierSetIndex = line_inliers1;
             bestInlierType = 'line';
-            bestInlierSet=X(line_inliers,:);
-            bestOutlierSet = X(~line_inliers,:);
+            
             V = P2 - P1;
             V_hat = V./norm(V);
             distances = X - P2;
-            projectedCoordinate = distances(line_inliers, :)*V_hat';
+            projectedCoordinate = distances(line_inliers1, :)*V_hat';
             bestEndPoints = [min(projectedCoordinate); max(projectedCoordinate)]*V_hat + repmat(candidates(2, :), [2, 1]);
         
             if isempty(bestEndPoints)
-                m = NaN;
-                b = NaN;
-                bestEndPoints = [NaN,NaN;NaN,NaN];
+                bestInlierSet = [];
+                bestInlierType = '';
+                bestInlierSetIndex = [];
                 continue
             end
-        end
-        
-        [circle_inliers, xc, yc, r, biggestGap] = circleDetector(X, d, P1, P2, P3);
-        
-        if biggestGap > 0.01
-            continue
-        end
-
-        if sum(circle_inliers) > size(bestInlierSet, 1) && sum(circle_inliers) > 1
+%         elseif sum(line_inliers2) > size(bestInlierSet, 1) && biggestGap2 < .2 % If second line is better
+%             bestInlierSet = X(line_inliers2,:);
+%             bestInlierSetIndex = line_inliers2;
+%             bestInlierType = 'line';
+%             
+%             V = P3 - P1;
+%             V_hat = V./norm(V);
+%             distances = X - P3;
+%             projectedCoordinate = distances(line_inliers2, :)*V_hat';
+%             bestEndPoints = [min(projectedCoordinate); max(projectedCoordinate)]*V_hat + repmat(candidates(2, :), [2, 1]);
+%             
+%         elseif sum(line_inliers3) > size(bestInlierSet, 1) && biggestGap3 < .2 % If third line is better
+%             bestInlierSet = X(line_inliers3,:);
+%             bestInlierSetIndex = line_inliers3;
+%             bestInlierType = 'line';
+%             
+%             V = P3 - P2;
+%             V_hat = V./norm(V);
+%             distances = X - P3;
+%             projectedCoordinate = distances(line_inliers3, :)*V_hat';
+%             bestEndPoints = [min(projectedCoordinate); max(projectedCoordinate)]*V_hat + repmat(candidates(2, :), [2, 1]);
+            
+        elseif sum(circle_inliers) > size(bestInlierSet, 1) && biggestGapC < 0.01 && sum(circle_inliers) > 3 % If circle is better
             inlier_points = X(circle_inliers,:);
             continue_flag = 0;
-            for i=1:size(inlier_points, 1)
+            for j=1:size(inlier_points, 1)*10
                 arc_candidates = datasample(inlier_points, 3, 'Replace', false);
                 C1 = arc_candidates(1,:);
                 C2 = arc_candidates(2,:);
                 C3 = arc_candidates(3,:);
 
-                [new_circle_inliers, new_xc, new_yc, new_r, new_biggestGap] = circleDetector(inlier_points, d/2, C1, C2, C3);
+                [new_circle_inliers, new_xc, new_yc, ~, ~] = circleDetector(inlier_points, d/2, C1, C2, C3);
 
                 center_offset = sqrt((xc-new_xc)^2 + (yc-new_yc)^2);
                 if center_offset > d || (sum(new_circle_inliers) ~= sum(circle_inliers))
@@ -64,60 +86,84 @@ function [bestInlierType, bestInlierSet, bestOutlierSet, m, b, bestEndPoints, C,
                 continue
             end
             
-            %%% THEN IT'S PROBABLY A CIRCLE
-            bestInlierType = 'circle';
             bestInlierSet = X(circle_inliers,:);
-            bestOutlierSet = X(~circle_inliers,:);
-            
-            C = [xc, yc];
-            R = r;
-        end
-        
-    end
-%     hold off
+            bestInlierSetIndex = circle_inliers;
+            bestInlierType = 'circle';
 
-%     
-%     if strcmp(bestInlierType, 'line')
-%         m = diff(bestEndPoints(:,2))/diff(bestEndPoints(:,1));
-%         b = bestEndPoints(1,2)-m*bestEndPoints(1,1);
-%         fitline_coefs=[m b];
-%     end
+            center = [xc, yc];
+            bestCircle = [center, r];
+        else
+            % Nothing was better than our current best
+            continue
+        end
+    end
+    
+    outliers = X(~bestInlierSetIndex,:);
+    
+    if strcmp(bestInlierType, 'line')
+        line = bestEndPoints;
+    elseif strcmp(bestInlierType, 'circle')
+        circle = bestCircle;
+    else
+        return
+    end
 end
 
 function [line_inliers, biggestGap] = lineDetector(X, d, P1, P2)
+    % Vector between the two points
     V = P2 - P1;
+    
+    % Unit vector between the two points
     V_hat = V./norm(V);
+    
+    % Orthogonal vector
     O = [-V(2) V(1)];
+    
+    % Orthogonal unit vector
     O_hat = O./norm(O);
+    
+    % Calculate perpendicular distances to the line
     distances = X - P2;
     perpendicular_distances = distances * O_hat';
+    
+    % Get the inliers (less than the threshold d)
     line_inliers = abs(perpendicular_distances) < d;
+    
+    % Calculate the biggest gap in the inliers
     biggestGap = max(diff(sort(distances(line_inliers,:)*V_hat')));
 end
 
 function [circle_inliers, xc, yc, r, biggestGap] = circleDetector(X, d, P1, P2, P3)
+    % Get x's and y's of the three sampled points
     P = [P1; P2; P3];
     Px = P(:,1);
     Py = P(:,2);
 
+    % Linear systems of equations to get the equation of the circle
     A = [Px, Py, ones(size(Px))];
     b = -Px.^2 - Py.^2;
     w = A \ b;
 
+    % Center and radius of the circle
     xc = -w(1)/2;
     yc = -w(2)/2;
     r = sqrt(xc.^2 + yc.^2 - w(3));
 
+    % Get the distance from every point to the circle
     distance_to_circle = abs(sqrt((X(:,1) - xc).^2 + (X(:,2) - yc).^2) - r);
     circle_inliers = (distance_to_circle < d);
     
+    % If there are fewer than 10 inliers, it's probably not a circle
     if sum(circle_inliers) < 10
         biggestGap = 0;
         return
     end
 
+    % List of points that are inliers
     inlier_points = X(circle_inliers);
 
+    % Find biggest gap %
+    % Choose a point on the arc as 0 degrees
     arc_point = datasample(inlier_points, 1);
     
     angles = [];
--- gauntlet.m
+++ gauntlet.m
@@ -1,152 +1,149 @@
 function gauntlet()
-    global scanner pubvel stopMessage message
-
+    global bump_sub pubvel stopMessage
+    
     init_NEATO();
     
     placeNeato(0, 0, 1, 0);
-    pause(2);
+    pause(2); % Wait for NEATO to fall
     
-    all_points = [];
+    positions = [0, 0, 0]; % Keeps track of position over time (x, y, theta)
     
-    f = figure;
-    
-    map = 0;
-    
+    % Create symbolic map for calculating gradient
     syms xx yy
-    symmap = 0;
+    symbolic_map = 0;
     
-    XX = -2:.08:3;
-    YY = -4:.08:1;
+    % Create numeric map for plotting contours and vector fields
+    numeric_map  = 0;
+    XX = -2:.1:3;
+    YY = -4:.1:1;
     [XX, YY] = meshgrid(XX, YY);
     
-    i = 0;
+    % Constants for gradient descent
+    lambda = 0.025;
+    delta = 1.1;
+    
+    f = figure;
+    p = [];
+    figure(f); hold on; xlim([-2 3]); ylim([-4 1]); axis equal;
+    
+    % Main movement loop
+    % % Each loop, the NEATO will
+    % % 1. SCAN
+    % % 2. MAP
+    % % 3. MOVE
+    % Will exit as soon as the NEATO bumps into something
     while (1)
-        i = i + 1;
-        if i == 2
-            break;
-        end
-        
-        %%% 1. SCAN
+        %%% SCAN %%%
         [r, theta] = LIDAR_scan();
-        [x, y] = pol2cart(theta, r);
+        
+        [x, y] = translate_to_G(r, theta, positions(end,3), positions(end,1:2));
         X = [x, y];
         
-        all_points = [all_points; round(x, 2), round(y, 2)];
-
-        figure(f)
-        plot(x, y, 'ks'); hold on; xlim([-2 3]); ylim([-4 1]); axis equal;
-        p = [];
+        p = [p; plot(x, y, 'ks')];
         
-        %%% 2. MAP
-        while size(X, 1) > 2
+        %%% MAP %%%
+        while size(X, 1) > 3
+            % Line will contain the best end points for the line
+            % Circle will contain the center and the radius of the circle
+            % Removes the inliers from X
+            [line, circle, X] = RANSAC(X, 1000, .025);
             disp(size(X));
-            [bestInlierType, bestInlierSet, X, m, b, bestEndPoints, C, R] = RANSAC(X, 1000, .02);
             
-            if strcmp(bestInlierType, 'None')
-                continue
-            end
-            
-            if strcmp(bestInlierType, 'line')
-                if isempty(bestEndPoints)
-                    continue
-                end
-                p = [p; plot(bestEndPoints(:,1), bestEndPoints(:,2), '-rs', 'LineWidth', 2)];
-                p = [p; plot(bestInlierSet(:,1), bestInlierSet(:,2), 'rs')];
-
-                map = map - log(sqrt((XX - bestEndPoints(1,1)).^2 + (YY - bestEndPoints(1,2)).^2));
-                map = map - log(sqrt((XX - bestEndPoints(2,1)).^2 + (YY - bestEndPoints(2,2)).^2));
-
-                symmap = symmap - log(sqrt((xx - bestEndPoints(1,1)).^2 + (yy - bestEndPoints(1,2)).^2));
-                symmap = symmap - log(sqrt((xx - bestEndPoints(2,1)).^2 + (yy - bestEndPoints(2,2)).^2));
-            else
-                p = [p; plot(bestInlierSet(:,1), bestInlierSet(:,2), 's')];
-                c = viscircles([C(1) C(2)], R, 'Color', 'green');
-                p = [p; c];
+            if ~isempty(line) % If RANSAC gave us a line
+                symbolic_map = symbolic_map + createLineSource(xx, yy, line);
+                numeric_map  = numeric_map  + createLineSource(XX, YY, line);
                 
-                map = map + log(sqrt((XX - C(1)).^2 + (YY - C(2)).^2));
-
-                symmap = symmap + log(sqrt((xx - C(1)).^2 + (yy - C(2)).^2));
+                p = [p; plot(line(:,1), line(:,2), '-rs', 'LineWidth', 2)];
             end
+            
+            if ~isempty(circle) % If RANSAC gave us a circle
+                symbolic_map = symbolic_map + createCircleSink(xx, yy, circle(1:2), circle(3));
+                numeric_map  = numeric_map  + createCircleSink(XX, YY, circle(1:2), circle(3));
+                
+                c = viscircles(circle(1:2), circle(3), 'Color', 'green', 'LineWidth', 2);
+                p = [p; c];
+            end
+            drawnow
         end
         
-        hold off
-    end
-%     
-%     
+        
+        [G1, G2] = gradient(numeric_map);
+        quiver(XX, YY, G1, G2);
+%         contour(XX, YY, numeric_map, 'ShowText', 'on');
+        drawnow
+        
+        return;
+        
+        %%% MOVE
+        current_position = positions(end,1:2);
+        current_angle = positions(end,3);
+        
+        g = gradient(symbolic_map, [xx, yy]);
+        
+        current_gradient = transpose(-1*double(subs(g, [xx, yy], {current_position(1,1), current_position(1,2)})));
+        
+        next_position = current_position + lambda.*current_gradient;
+        
+        v = next_position - current_position;
+        
+        % Causes the NEATO to rotate to the new angle
+        new_angle = atan2(v(2), v(1));
+        rotate(new_angle - current_angle);
+        
+        % Causes the NEATO to travel to the next point
+        travel_distance = norm(v);
+        travel(travel_distance);
+        
+        % If the NEATO has bumped into anything, we should exit
+        bumpMessage = receive(bump_sub);
+        if any(bumpMessage.Data)
+            send(pubvel, stopMessage);
+            break;
+        end
 
-%     
-%     syms xx yy;
-%     
-%     XX = -2:.08:3;
-%     YY = -4:.08:1;
-%     [XX, YY] = meshgrid(XX, YY);
-%     
-%     all_points = [round(x, 2), round(y, 2)];
-%     
-%     map = 0;
-%     symmap = 0;
-%     
-%     close all;
-%     f = figure;
-%     plot(x, y, 'ks'); hold on; xlim([-2 3]); ylim([-4 1]); axis equal;
-%     
-%     while size(X, 1) > 2
-%         [fitline_coefs,bestInlierSet,X,bestEndPoints] = RANSAC(X, 100, .01);
-%         
-%         if any(isnan(bestEndPoints))
-%             break
-%         end
-%         
-%         p = plot(bestEndPoints(:,1), bestEndPoints(:,2), '-rs', 'LineWidth', 2);
-%         p = [p; plot(bestInlierSet(:,1), bestInlierSet(:,2), 'rs')];
-%         
-%         m = fitline_coefs(1); b = fitline_coefs(2);
-%         
-%         map = map - log(sqrt((XX - bestEndPoints(1,1)).^2 + (YY - bestEndPoints(1,2)).^2));
-%         map = map - log(sqrt((XX - bestEndPoints(2,1)).^2 + (YY - bestEndPoints(2,2)).^2));
-%         
-%         symmap = symmap - log(sqrt((xx - bestEndPoints(1,1)).^2 + (yy - bestEndPoints(1,2)).^2));
-%         symmap = symmap - log(sqrt((xx - bestEndPoints(2,1)).^2 + (yy - bestEndPoints(2,2)).^2));
-%     end
-%     
-%     g = gradient(symmap, [xx, yy]);
-%     
-%     current_gradient = double(subs(g, [xx, yy], {0, 0}));
-%     
-%     new_angle = atan2(current_gradient(2), current_gradient(1));
-%     
-%     rotate(new_angle);
-    
-% %     contour(XX, YY, map, 'ShowText', 'on');
-% %     
-% %     XX = -2:.5:3;
-% %     YY = -4:.5:1;
-% %     [XX, YY] = meshgrid(XX, YY);
-% % 
-% %     G1 = subs(g(1), [xx, yy], {XX, YY});
-% %     G2 = subs(g(2), [xx, yy], {XX, YY});
-% %     
-% %     quiver(XX, YY, G1, G2, 'LineWidth', 1.5);
+        % Update the list of positions, and the lambda
+        positions = [positions; next_position, new_angle];
+        lambda = lambda*delta;
+        
+        % Reset the numeric and symbolic maps
+        symbolic_map = 0; numeric_map = 0;
+        delete(p);
+    end
     
     hold off
     destroy()
+    disp("Done");
 end
 
-function n = make_peak(P)
-    n = -log(sqrt((xx - P(1)).^2 + (yy - P(2)).^2));
+function term = createCircleSink(x, y, P, r)
+    term = 0;
+    for theta = 0:0.1*pi:2*pi
+        a = r*cos(theta) + P(1);
+        b = r*sin(theta) + P(2);
+
+        term = term + log(sqrt((x - a).^2 + (y - b).^2));
+    end
 end
 
-function n = make_valley(P)
-    syms xx yy
-    n = log(sqrt((xx - P(1)).^2 + (yy - P(2)).^2));
-    disp(n);
+function term = createLineSource(x, y, endpoints)
+    term = 0;
+    
+    length = norm(endpoints(1,:) - endpoints(2,:));
+    spacing = linspace(0, 1, ceil(length * 10));
+    
+    P1 = endpoints(1,:); P2 = endpoints(2,:);
+    
+    for n=1:size(spacing, 2)
+        point = P1 + spacing(n)*(P2 - P1);
+        term = term - log(sqrt((x - point(1,1)).^2 + (y - point(1,2)).^2));
+    end
 end
 
 function init_NEATO()
     % Initializes the NEATO
     %   Uses rosinit(), sets up LIDAR scanner, wheel velocity
     %   controller, etc.
-    global scanner pubvel stopMessage message
+    global scanner pubvel stopMessage message sub_states sub_encoders bump_sub
 
     if (~ros.internal.Global.isNodeActive)
         rosshutdown()
@@ -166,6 +163,12 @@ function init_NEATO()
 
     % Create a message for moving the NEATO
     message = rosmessage(pubvel);
+    
+    sub_states = rossubscriber('/gazebo/model_states', 'gazebo_msgs/ModelStates');
+    
+    sub_encoders = rossubscriber('/encoders');
+    
+    bump_sub = rossubscriber('/bump');
 end
 
 function [r, theta] = LIDAR_scan()
@@ -182,9 +185,9 @@ function [r, theta] = LIDAR_scan()
 end
 
 function travel(distance)
-    global pubvel message stopMessage
+    global pubvel message stopMessage bump_sub
     
-    wheel_speed = .2;
+    wheel_speed = .1;
 
     start = rostime('now');
 
@@ -194,6 +197,13 @@ function travel(distance)
     while (1)
         current = rostime('now');
         elapsed = current - start;
+        
+        bumpMessage = receive(bump_sub);
+        if any(bumpMessage.Data)
+            send(pubvel, stopMessage);
+            break;
+        end
+
 
         if elapsed.seconds >= abs(distance/wheel_speed)
             send(pubvel, stopMessage);
@@ -204,19 +214,17 @@ end
 
 function rotate(theta)
     global pubvel message stopMessage
-    
-    wheel_speed = .2;
+
+    wheel_speed = .1;
     wheel_base = 0.235;
 
-    message.Data = [wheel_speed, sign(theta) * wheel_speed];
+    message.Data = [-1*sign(theta)*wheel_speed, sign(theta)*wheel_speed];
     send(pubvel, message);
 
     start = rostime('now');
     while (1)
         current = rostime('now');
         elapsed = current - start;
-        
-        disp(elapsed.seconds)
 
         if elapsed.seconds >= abs(theta/(2*wheel_speed / wheel_base))
             send(pubvel, stopMessage);
@@ -227,13 +235,6 @@ end
 
 function destroy()
     rosshutdown();
-end
-
-function p = plot_circle(r, x, y)
-%     theta = 0:pi/50:2*pi;
-%     xunit = r * cos(theta) + x;
-%     yunit = r * sin(theta) + y;
-%     p = plot(xunit, yunit, '-s');
 end
 
 function placeNeato(posX, posY, headingX, headingY)
@@ -254,4 +255,9 @@ function placeNeato(posX, posY, headingX, headingY)
 
     % put the robot in the appropriate place
     ret = call(svc, msg);
+end
+
+function [x, y] = translate_to_G(r, theta, phi, pos)
+    x = r.*cos(theta + phi) - 0.084 .* cos(phi) + pos(1);
+    y = r.*sin(theta + phi) - 0.084 .* sin(phi) + pos(2);
 end
